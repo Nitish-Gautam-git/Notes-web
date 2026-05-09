2efa57c5bde8eb1c4eead4d1fa60b100422adfff
nitish-99-tech <nitishgautam917@gmail.com>
2026-05-09 15:13:49 +0530

    implement local storage for data persistence

--- index.js
+++ index.js
@@ -1,136 +1,177 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let flag = true;
-  function saveNotes(e, title, notes, overlay) {
-    if (!flag) return;
-    console.log("1st worked !");
-    const notesTitle = document.createElement("h4");
-    notesTitle.innerText = title.value;
+    console.log() ;
+    const wrapper = document.querySelector(".wrapper");
+    const plusIcon = document.getElementsByClassName("plus-icon")[0];
+    function saveToDisk(){
+        localStorage.setItem('allNotes' , JSON.stringify(allNotes)) ;
+    }
 
-    const notesDesc = document.createElement("p");
-    notesDesc.innerText = notes.value;
+    function renderUI(){
+        
+        wrapper.replaceChildren() ;
+        wrapper.insertAdjacentElement('afterbegin' , plusIcon) ;
+        
+        if(allNotes.length === 0 ) return ;
+        allNotes.forEach((element , i) => {
+            const notesTitle = document.createElement("h4");
+            notesTitle.innerText = element[0] ;
 
-    const textWrap = document.createElement("div");
-    textWrap.classList.add("text-wrap");
+            const notesDesc = document.createElement("p");
+            notesDesc.innerText =  element[1];
+            
+            const textWrap = document.createElement("div");
+            textWrap.classList.add("text-wrap");
+            textWrap.dataset.id = i ;
+            
+            textWrap.appendChild(notesTitle);
+            textWrap.appendChild(notesDesc);
+            
+            wrapper.appendChild(textWrap);
+        });
+        
+    }
+    let data = localStorage.getItem('allNotes') ;
+    let allNotes =(data)? JSON.parse(data) : [] ;
 
-    textWrap.appendChild(notesTitle);
-    textWrap.appendChild(notesDesc);
+    renderUI();
+    let flag = true;
+    
+    let openPopup = (e) => {
+        const textWrap = e.target.closest(".text-wrap");
 
-    wrapper.appendChild(textWrap);
-    overlay.remove();
-  }
+        if (!textWrap) return; // clicked on wrapper or first box , closest will return null
 
-  let openPopup = (e) => {
-    const textWrap = e.target.closest(".text-wrap");
+        const overlay = document.createElement("div");
+        overlay.classList.add("overlay");
 
-    if (!textWrap) return; // clicked on wrapper or first box , closest will return null
+        const popUp = document.createElement("div");
+        popUp.classList.add("popup");
+        popUp.innerHTML = textWrap.innerHTML;
 
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
+        const deleteNote = document.createElement("button");
+        deleteNote.innerText = "delete";
 
-    const popUp = document.createElement("div");
-    popUp.classList.add("popup");
-    popUp.innerHTML = textWrap.innerHTML;
+        const editNote = document.createElement("button");
+        editNote.innerText = "edit";
 
-    const deleteNote = document.createElement("button");
-    deleteNote.innerText = "delete";
+        const closePopup = document.createElement("button");
+        closePopup.innerText = "close";
 
-    const editNote = document.createElement("button");
-    editNote.innerText = "edit";
+        const end = document.createElement("div");
+        end.appendChild(editNote);
+        end.appendChild(deleteNote);
+        end.appendChild(closePopup);
 
-    const closePopup = document.createElement("button");
-    closePopup.innerText = "close";
+        deleteNote.addEventListener("click", (e2) => {
+            allNotes.splice(textWrap.dataset.id , 1) ;
+            
+            textWrap.remove();
+            overlay.remove();
+            saveToDisk() ;
+            renderUI() ;
 
-    const end = document.createElement("div");
-    end.appendChild(editNote);
-    end.appendChild(deleteNote);
-    end.appendChild(closePopup);
+        });
+        closePopup.addEventListener("click", () => overlay.remove());
 
-    deleteNote.addEventListener("click", (e2) => {
-      textWrap.remove();
-      overlay.remove();
-    });
-    closePopup.addEventListener("click", () => overlay.remove());
-    editNote.addEventListener("click", () => {
-      flag = false;
-      addNotes();
+        editNote.addEventListener("click", () => {
+            flag = false;
+            addNotes();
 
-      const inputWrapper = document.getElementsByClassName("input-wrapper")[0];
-      let inputChild = inputWrapper.children;
+            const inputWrapper = document.getElementsByClassName("input-wrapper")[0];
+            
+            let prevTitle = textWrap.firstElementChild.innerText   ;
+            let prevNotes = textWrap.lastElementChild.innerText   ;
+            
+            let inputChild = inputWrapper.children;
+            inputChild[1].value = prevTitle ;
+            inputChild[3].value = prevNotes ;
 
-      inputChild[1].value = textWrap.firstElementChild.innerText;
-      inputChild[3].value = textWrap.lastElementChild.innerText;
-      const submit = document.querySelector(".save");   
+            const saveButton = document.querySelector(".save");
 
-      submit.addEventListener("click", () => {
-        textWrap.firstElementChild.innerText = inputChild[1].value;
-        textWrap.lastElementChild.innerText = inputChild[3].value;
+            saveButton.addEventListener("click", () => {
+                if(inputChild[1].value.trim() == '' ||  inputChild[3].value.trim() == '' ){
+                    return window.alert('Title or Note can\'t be empty') ;
+                }
 
-        console.log("2nd worked\!");
-        flag = true;
-      });
+                allNotes.splice( textWrap.dataset.id , 1 , [inputChild[1].value, inputChild[3].value] ) ; 
+                
 
-      overlay.remove();
-    });
+                inputWrapper.closest('.overlay').remove();
+                flag = true;
+                saveToDisk() ;
+                renderUI() ;
+            });
+            
+            overlay.remove();
+        });
 
-    overlay.appendChild(popUp);
-    popUp.appendChild(end);
-    document.body.appendChild(overlay);
-  };
+        overlay.appendChild(popUp);
+        popUp.appendChild(end);
+        document.body.appendChild(overlay);
+    };
 
-  const wrapper = document.querySelector(".wrapper");
-  wrapper.addEventListener("click", openPopup);
+    wrapper.addEventListener("click", openPopup);
 
-  let addNotes = (e) => {
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
 
-    const inputWrapper = document.createElement("div");
-    const title = document.createElement("input");
-    title.type = "text";
-    title.id = "title";
+    let addNotes = (e) => {
+        const overlay = document.createElement("div");
+        overlay.classList.add("overlay");
 
-    const titleLabel = document.createElement("label");
-    titleLabel.innerText = "Title";
-    titleLabel.htmlFor = "title";
+        const inputWrapper = document.createElement("div");
+        const title = document.createElement("input");
+        title.type = "text";
+        title.id = "title";
 
-    const notes = document.createElement("textarea");
-    notes.cols = "30";
-    notes.rows = "8";
-    notes.id = "notes";
+        const titleLabel = document.createElement("label");
+        titleLabel.innerText = "Title";
+        titleLabel.htmlFor = "title";
 
-    const notesLabel = document.createElement("label");
-    notesLabel.innerText = "Notes";
-    notesLabel.htmlFor = "notes";
+        const notes = document.createElement("textarea");
+        notes.cols = "30";
+        notes.rows = "8";
+        notes.id = "notes";
 
-    const submit = document.createElement("button");
-    submit.classList.add("save");
-    submit.innerText = "Save";
+        const notesLabel = document.createElement("label");
+        notesLabel.innerText = "Notes";
+        notesLabel.htmlFor = "notes";
 
-    const closePopup = document.createElement("button");
-    closePopup.innerText = "close";
+        const saveButton = document.createElement("button");
+        saveButton.classList.add("save");
+        saveButton.innerText = "Save";
 
-    submit.addEventListener("click", (e) => {
-      saveNotes(e, title, notes, overlay);
-      overlay.remove();
-    });
-    closePopup.addEventListener("click", () => overlay.remove());
+        const closePopup = document.createElement("button");
+        closePopup.innerText = "close";
 
-    const end = document.createElement("div");
-    end.appendChild(submit);
-    end.appendChild(closePopup);
+        saveButton.addEventListener("click", (e) => {
+            if(!flag) return  ;
 
-    inputWrapper.appendChild(titleLabel);
-    inputWrapper.appendChild(title);
-    inputWrapper.appendChild(notesLabel);
-    inputWrapper.appendChild(notes);
-    inputWrapper.appendChild(end);
+            if(title.value.trim() == '' ||  notes.value.trim() == '' ){
+                return window.alert('Title or Note can\'t be empty') ;
+            }
+            allNotes.push( [title.value , notes.value ]) ;
+            overlay.remove();
+            saveToDisk() ;
+            renderUI() ;
 
-    inputWrapper.classList.add("input-wrapper");
+        });
 
-    overlay.appendChild(inputWrapper);
-    document.body.appendChild(overlay);
-  };
+        closePopup.addEventListener("click", () => {flag = true; overlay.remove() ;});
 
-  const plusIcon = document.getElementsByClassName("plus-icon")[0];
-  plusIcon.addEventListener("click", addNotes);
+        const end = document.createElement("div");
+        end.appendChild(saveButton);
+        end.appendChild(closePopup);
+
+        inputWrapper.appendChild(titleLabel);
+        inputWrapper.appendChild(title);
+        inputWrapper.appendChild(notesLabel);
+        inputWrapper.appendChild(notes);
+        inputWrapper.appendChild(end);
+
+        inputWrapper.classList.add("input-wrapper");
+
+        overlay.appendChild(inputWrapper);
+        document.body.appendChild(overlay);
+    };
+
+    plusIcon.addEventListener("click", addNotes);
 });
